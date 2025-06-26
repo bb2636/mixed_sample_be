0a0404ced2b4dfd9738e04c286d2ba397ebce2cd
bb2636 <leesubin0322@gmail.com>
2025-06-26 12:23:50 +0900

    first

---
+++ src/main/java/com/example/springbe/SpringBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbe/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.example.springbe.controller;
+
+import com.example.springbe.dto.MovieDto;
+import com.example.springbe.dto.MoviesDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", methods= RequestMethod.GET)
+public class MainController {
+    @GetMapping
+    public MoviesDto hello() {
+        MoviesDto movies = new MoviesDto();
+        MovieDto movieDto = new
+                MovieDto(1,"testMovie","https://img.yts.mx/assets/images/movies/kpop_demon_hunters_2025/medium-cover.jpg",
+                "summaryTest");
+                List<MovieDto> moviesList = new ArrayList<>();
+        moviesList.add(movieDto);
+        movies.setMovies(moviesList);
+        return movies;
+    }
+}
